32153e3b13149defa9458e760890e01c1a542e60
Dominik Santoro <174945216+dhz-77@users.noreply.github.com>
2024-09-11 16:36:11 +0700

    Add function to display buttons clicked
    If the user clicks one or multiple buttons, the associated number(s)
    have to show on the display.
    
    The display value is stored in a variable.

--- main.js
+++ main.js
@@ -34,3 +34,63 @@ const operate = function(firstNumber, secondNumber, operator) {
         divide(firstNumber, secondNumber);
     }
 }
+
+function display(number) { 
+    const result = document.querySelector("#result")
+    result.value += number 
+
+    const displayValue = result.value;
+} 
+
+setEventListener();
+
+function setEventListener() {
+    let elementsArray = document.querySelectorAll('input[type="button"]');
+    
+    elementsArray.forEach(function(element) {
+        element.addEventListener("click", () => {
+            if(element.value === "1") {
+                display("1");
+            }
+            if(element.value === "2") {
+                display("2");
+            }
+            if(element.value === "3") {
+                display("3");
+            }
+            if(element.value === "4") {
+                display("4");
+            }
+            if(element.value === "5") {
+                display("5");
+            }
+            if(element.value === "6") {
+                display("6");
+            }
+            if(element.value === "7") {
+                display("7");
+            }
+            if(element.value === "8") {
+                display("8");
+            }
+            if(element.value === "9") {
+                display("9");
+            }
+            if(element.value === "0") {
+                display("0");
+            }
+            if(element.value === "+") {
+                display("+");
+            }
+            if(element.value === "−") {
+                display("−");
+            }
+            if(element.value === "×") {
+                display("×");
+            }
+            if(element.value === "÷") {
+                display("÷");
+            }
+        });
+    });
+}
